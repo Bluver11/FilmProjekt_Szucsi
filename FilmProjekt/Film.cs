56aeb18caed7b2737c45f360b84d31b96493ff1d
Szupkai Ricsi <szupkai.ricsi@petrik.hu>
2023-09-21 10:19:33 +0200

    FIlm osztály létrehozzása

---
+++ FilmProjekt/Film.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmProjekt
+{
+	internal class Film
+	{
+		private int helyezes;
+		private string cim;
+		private int ev;
+		private int ido;
+		private List<string> mufaj;
+	}
+}
